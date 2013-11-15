12e94d6d1fa841816d2b2be3ad9eaeed53b842a7
oadam <oadam@efront.com>
2013-11-15 17:32:17 +0100

    carte are enums

--- src/main/java/jeu/Carte.java
+++ src/main/java/jeu/Carte.java
@@ -8,49 +8,65 @@ import javax.swing.JLabel;
 // Referenced classes of package jeu:
 //            Couleur, BoutonCarte
 
-public class Carte
+public enum Carte
 {
-
+	CARREAU_1(1, Couleur.carreau),
+	CARREAU_2(2, Couleur.carreau),
+	CARREAU_3(3, Couleur.carreau),
+	CARREAU_4(4, Couleur.carreau),
+	CARREAU_5(5, Couleur.carreau),
+	CARREAU_6(6, Couleur.carreau),
+	CARREAU_7(7, Couleur.carreau),
+	CARREAU_8(8, Couleur.carreau),
+	CARREAU_9(9, Couleur.carreau),
+	CARREAU_10(10, Couleur.carreau),
+	PIQUE_1(1, Couleur.pique),
+	PIQUE_2(2, Couleur.pique),
+	PIQUE_3(3, Couleur.pique),
+	PIQUE_4(4, Couleur.pique),
+	PIQUE_5(5, Couleur.pique),
+	PIQUE_6(6, Couleur.pique),
+	PIQUE_7(7, Couleur.pique),
+	PIQUE_8(8, Couleur.pique),
+	PIQUE_9(9, Couleur.pique),
+	PIQUE_10(10, Couleur.pique),
+	TREFLE_1(1, Couleur.trefle),
+	TREFLE_2(2, Couleur.trefle),
+	TREFLE_3(3, Couleur.trefle),
+	TREFLE_4(4, Couleur.trefle),
+	TREFLE_5(5, Couleur.trefle),
+	TREFLE_6(6, Couleur.trefle),
+	TREFLE_7(7, Couleur.trefle),
+	TREFLE_8(8, Couleur.trefle),
+	TREFLE_9(9, Couleur.trefle),
+	TREFLE_10(10, Couleur.trefle),
+	COEUR_1(1, Couleur.coeur),
+	COEUR_2(2, Couleur.coeur),
+	COEUR_3(3, Couleur.coeur),
+	COEUR_4(4, Couleur.coeur),
+	COEUR_5(5, Couleur.coeur),
+	COEUR_6(6, Couleur.coeur),
+	COEUR_7(7, Couleur.coeur),
+	COEUR_8(8, Couleur.coeur),
+	COEUR_9(9, Couleur.coeur),
+	COEUR_10(10, Couleur.coeur),
+	;
+	
     private final int valeur;
     private final Couleur couleur;
     
+    
+    
     private static Carte paquet[][];
     static {
-    	paquet = new Carte[10][4];
-    	for(int i = 0; i < 10; i++)
-    	{
-    		paquet[i][0] = new Carte(i + 1, Couleur.carreau);
-    		paquet[i][1] = new Carte(i + 1, Couleur.coeur);
-    		paquet[i][2] = new Carte(i + 1, Couleur.pique);
-    		paquet[i][3] = new Carte(i + 1, Couleur.trefle);
-    	}
+    	paquet = new Carte[10][Couleur.values().length];
+    	for (Carte carte : Carte.values()) {
+			paquet[carte.valeur - 1][carte.couleur.ordinal()] = carte;
+		}
     }
     public static Carte obtenirCarte(int a, Couleur couleur)
     {
-        int indiceCouleur;
-        switch(couleur)
-        {
-        case carreau: // '\002'
-            indiceCouleur = 0;
-            break;
-
-        case coeur: // '\003'
-            indiceCouleur = 1;
-            break;
-
-        case pique: // '\004'
-            indiceCouleur = 2;
-            break;
-
-        case trefle: // '\001'
-            indiceCouleur = 3;
-            break;
-
-        default:
-            indiceCouleur = 60;
-            break;
-        }
-        return paquet[a - 1][indiceCouleur];
+        return paquet[a - 1][couleur.ordinal()];
     }
 
     private Carte(int a, Couleur b)
